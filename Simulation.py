c929742faad84878c754b7ac2f314c0cfe4fbe21
Rainel Fernandez <rainelfrnndz@gmail.com>
2022-12-30 16:40:38 -0500

    Squashed commit of the following:
    commit 912fbc684787810b100e8569db43c7673dcf137a
    Author: Rainel Fernandez <rainelfrnndz@gmail.com>
    Date:   Fri Dec 30 16:38:36 2022 -0500
    
        complete soldiers return to camps
    
    commit 86ed40be6d822e0128dc79efca590755a457c466
    Author: Rainel Fernandez <rainelfrnndz@gmail.com>
    Date:   Mon Dec 26 15:10:23 2022 -0500
    
        asigned camps to each soldier
    
        its necessary to make the soldiers return to the camps
    
    commit c38c99925b11a4e09718d7a5b8ee364297b71492
    Author: Rainel Fernandez <rainelfrnndz@gmail.com>
    Date:   Sun Dec 25 21:54:45 2022 -0500
    
        camps created!
    
    commit 0f6d185a1ae674ab29f8fb7739bc740c43927f14
    Author: Rainel Fernandez <rainelfrnndz@gmail.com>
    Date:   Sun Dec 25 15:03:58 2022 -0500
    
        armors created

--- Simulation.py
+++ Simulation.py
@@ -1,3 +1,4 @@
+from sympy import primefactors
 from Soldier import *
 from Battlefield import *
 import Graph_simulation
@@ -12,15 +13,16 @@ def start_simulation(map_rows, map_cols, amount_army_a, amount_army_b, rounds):
     arms = armors
     # !revisar bien esto!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     start_price = ((amount_army_a + amount_army_b) // 3) * 30
-    # lista que contendra todos los soldados de la simulacion
     soldiers: List[Soldier] = []
-    map = Map(map_rows, map_cols)  # mapa de la simulacion
+    visits = amount_army_a
+    map = Map(map_rows, map_cols, visits)  # mapa de la simulacion
     army_a = create_soldier(amount_army_a, 'A', map)
     dress_army(army_a, start_price, min_price.price)
     army_b = create_soldier(amount_army_b, 'B', map)
     dress_army(army_b, start_price, min_price.price)
     auxiliar.marge_armys(army_a, army_b, soldiers)
     asign_camps_to_soldiers(soldiers, map.get_camps())
+    check_possition(soldiers, map.camps, map)
     # se ordenan los soldados de ambos ejercitos segun su velocidad
     soldiers.sort(key=lambda soldier: soldier.speed, reverse=True)
     soldiers_a = []
@@ -39,45 +41,48 @@ def start_simulation(map_rows, map_cols, amount_army_a, amount_army_b, rounds):
 
     mean, var = rain.calc_mean_var(country)
     iterations = [i for i in range(rounds)]
+    print("Mapa inicial:")
+    print_map(map)
+    print('\n')
     while rounds > 0:
         rain_mm = rain.get_rain_prob(mean, var, month)
         if rain_mm[0] > 50:
             for sol in soldiers:
                 rain.apply_debuf(sol)
 
-        # contar a todos los soldados por ejercito para graficar luego
         soldiers_a, soldiers_b = Graph_simulation.count_soldiers_alive(
             soldiers_a, soldiers_b, map)
-        # mandar a todo los soldados a atcar
         for soldier in soldiers:
             if soldier.weapon_life <= 0:
-                pass  # retornar al campamento
-            attacked = soldier.attack_strategy_one(map)
-            if not attacked:
-                #start = time.time()
-                next_pos = auxiliar.bfs(map.get_battlefield(), soldier)
-                if next_pos != None:
-                    # !!!!! aqui voy a cambiar cosas pa q si tienen energia se muevan, sino de un pasito nama
-                    spend_energy = soldier.use_energy(next_pos)
-                #print(time.time() - start, "++++++++++++")
-                    if soldier.get_energy() - spend_energy >= 0:
-                        soldier.move_soldier(
-                            next_pos[0], next_pos[1], map.get_battlefield())
-
-        # luego de cada ronda, donde todos los soldados ataquen, se debe quitar de la lista de los soldados aquellos que tengan vida <= 0
+                if soldier.camp.state == True:
+                    soldier.return_to_camp(map)
+                    continue
+            else:
+                attacked = soldier.attack_strategy_one(map)
+                if not attacked:
+                    next_pos = auxiliar.bfs(map.get_battlefield(), soldier)
+                    if next_pos != None:
+                        # !!!!! aqui voy a cambiar cosas pa q si tienen energia se muevan, sino de un pasito nama
+                        spend_energy = soldier.use_energy(next_pos)
+                        if soldier.get_energy() - spend_energy >= 0:
+                            soldier.move_soldier(
+                                next_pos[0], next_pos[1], map.get_battlefield())
+        if rounds % 5 == 0:
+            print_map(map)
+        print('\n')
         remove_soldier_form_list(soldiers, army_a, army_b)
-        # lo mismo debe hacerse en el campo de batalla
         map.remove_fallen_soldiers()
         # if len(army_a) <= len(army_b)*0.3  or len(army_b) <= len(army_a)*0.3:   ## revisar esto bien
         #     #print("No se acabo x las rondas de simulacion")
         #     break
         if len(army_a) == 0 or len(army_b) == 0:
             break
-        # dar energia a todos los soldados
         soldier_energy(soldiers)
         if rain_mm[0] > 50:
             for sol in soldiers:
                 rain.remove_debuff(sol)
+        for camp in map.camps:
+            fix_weapons_and_restore_stats(camp, map)
         rounds -= 1
 
     auxiliar.fix_axes(iterations, soldiers_a, soldiers_b)
@@ -127,12 +132,38 @@ def asign_camps_to_soldiers(soldiers, camps):
 def print_map(map):
     for x in range(map.get_row()):
         for y in range(map.get_col()):
-            print(map.get_battlefield()[x][y], end=' ', sep=' ')
+            if map.get_battlefield()[x][y] == None:
+                print("_", end=' ', sep=' ')
+            else:
+                print(map.get_battlefield()[x][y], end=' ', sep=' ')
         print('\n')
 
 
-n = 0
-while (n < 20):
-    print(n)
-    start_simulation(20, 20, 50, 50, 150)
-    n += 1
+def check_possition(soldiers, camps, map):
+    for sol in soldiers:
+        if (sol.get_pos_x(), sol.get_pos_y()) in (camps[0].n_cells) or (sol.get_pos_x(), sol.get_pos_y()) in (camps[1].n_cells):
+            if (sol.get_pos_x() + 1) < map.get_row() and map.battlefield[sol.get_pos_x() + 1][sol.get_pos_y()] == None:
+                map.battlefield[sol.get_pos_x()][sol.get_pos_y()] = None
+                sol.pos_x = sol.get_pos_x() + 1
+                map.battlefield[sol.get_pos_x()][sol.get_pos_y()] = sol
+            if (sol.get_pos_x() - 1) < map.get_row() and map.battlefield[sol.get_pos_x() - 1][sol.get_pos_y()] == None:
+                map.battlefield[sol.get_pos_x()][sol.get_pos_y()] = None
+                sol.pos_x = sol.get_pos_x() - 1
+                map.battlefield[sol.get_pos_x()][sol.get_pos_y()] = sol
+            if (sol.get_pos_y() + 1) < map.get_col() and map.battlefield[sol.get_pos_x()][sol.get_pos_y() + 1] == None:
+                map.battlefield[sol.get_pos_x()][sol.get_pos_y()] = None
+                sol.pos_y = sol.get_pos_y() + 1
+                map.battlefield[sol.get_pos_x()][sol.get_pos_y()] = sol
+            if (sol.get_pos_x() + 1) < map.get_col() and map.battlefield[sol.get_pos_x()][sol.get_pos_y() - 1] == None:
+                map.battlefield[sol.get_pos_x()][sol.get_pos_y()] = None
+                sol.pos_y = sol.get_pos_y() - 1
+                map.battlefield[sol.get_pos_x()][sol.get_pos_y()] = sol
+
+
+start_simulation(20, 20, 40, 40, 100)
+
+# n = 0
+# while (n < 20):
+#     print(n)
+#     start_simulation(20, 20, 50, 50, 150)
+#     n += 1
